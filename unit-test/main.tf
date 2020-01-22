2b49c6dca4ff3ef86b8d5c35468773eda175be13
Prakash Manglanathan <pmnathan@gmail.com>
2020-01-22 16:01:28 -0500

    Update main.tf

--- unit-test/main.tf
+++ unit-test/main.tf
@@ -51,7 +51,7 @@ module "ec2-instance" {
   instance_count         = 1
 
   ami                    = "ami-03fe4ab7cbc8fe59d"
-  instance_type          = "t2.medium"
+  instance_type          = "t2.micro"
   key_name               = "Prakash Test"
   monitoring             = true
   vpc_security_group_ids = [aws_security_group.lc_security_group.id]
