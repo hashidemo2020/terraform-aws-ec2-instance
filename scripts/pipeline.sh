687ea168c9cca63d90755cc2964c18fe52ecdcf0
Manjeet <manjeet@hashicorp.com>
2020-01-22 11:47:30 -0500

    First Commit Module EC2

---
+++ scripts/pipeline.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Update Deadline
+export POLICY_ID='polset-4mo721h3BJLeRda5'
+export VAR_ID='var-SGkqCCEuZoAc7MkT'
+export KEY='push_date'
+export VALUE=$(date --utc +%FT%TZ)
+curl -v -sk \
+  --request PATCH \
+  --data "{\"data\":{\"id\":\"$VAR_ID\",\"attributes\":{\"key\":\"$KEY\",\"value\":\"$VALUE\",\"category\":\"policy-set\",\"description\":null,\"hcl\":false,\"sensitive\":false,\"read-only\":false,\"relationships\":{\"configurable\":{\"data\":{\"type\":\"policy-sets\",\"id\":\"$POLICY_ID\"}}},\"type\":\"vars\"}}}" \
+  -H "Content-type: application/vnd.api+json" \
+  -H "Authorization: Bearer $TFE_TOKEN" \
+  https://app.terraform.io/api/v2/policy-sets/$POLICY_ID/parameters/$VAR_ID
+
+# Update version
+# Remove leading 'v' from version number
+export VAR_ID='var-gNi9dD6vjNuM34Rp'
+export KEY='version'
+export VALUE=$(echo $TRAVIS_TAG | sed -E 's/v?([0-9\.]+)/\1/')
+curl -v -sk \
+  --request PATCH \
+  --data "{\"data\":{\"id\":\"$VAR_ID\",\"attributes\":{\"key\":\"$KEY\",\"value\":\"$VALUE\",\"category\":\"policy-set\",\"description\":null,\"hcl\":false,\"sensitive\":false,\"read-only\":false,\"relationships\":{\"configurable\":{\"data\":{\"type\":\"policy-sets\",\"id\":\"$POLICY_ID\"}}},\"type\":\"vars\"}}}" \
+  -H "Content-type: application/vnd.api+json" \
+  -H "Authorization: Bearer $TFE_TOKEN" \
+  https://app.terraform.io/api/v2/policy-sets/$POLICY_ID/parameters/$VAR_ID
